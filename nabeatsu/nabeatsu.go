ae4581c955f575c13a3742ee7a9467f794b734b6
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2021-12-15 23:39:50 +0900

    ナベアツの限界を無量大数まで引き上げる

--- nabeatsu/nabeatsu.go
+++ nabeatsu/nabeatsu.go
@@ -30,14 +30,33 @@ func IsFool(s string) bool {
 
 // BuildFoolMessage 3の倍数と3のつく数字のときだけ阿呆になった数字の読み方をします。それ以外のケースではオリジナルの文字列を返します。
 func BuildFoolMessage(s string) string {
-	if len(s) > 4 {
-		return "現在ﾅﾍﾞｱﾂは10,000以上の数を計算できません"
+	if len(s) >= 73 { // 無量大数より上の単位は存在しないので無限大ということにしておく
+		return "ﾑｹﾞﾝﾀﾞｧｲwww"
 	}
 	if !IsFool(s) {
 		return s
 	}
 
-	return buildFoolMessageUnder10Thousand(s)
+	// 10,000未満
+	if len(s) <= 4 {
+		return buildFoolMessageUnder10Thousand(s)
+	}
+	msg := buildFoolMessageUnder10Thousand(s[len(s)-4:])
+	s = s[:len(s)-4]
+
+	// 以降、適切な接頭辞を追加しながら構築
+	for i := 0; i < len(prefix) && len(s) > 0; i++ {
+		p := prefix[i]
+		offset := len(s) - 4
+		if offset < 0 {
+			offset = 0
+		}
+
+		msg = buildFoolMessageWithHighPrefix(s[offset:], p) + msg
+		s = s[:offset]
+	}
+
+	return msg
 }
 
 func buildFoolMessageUnder10Thousand(s string) string {
@@ -102,6 +121,83 @@ func buildFoolMessageUnder10Thousand(s string) string {
 	return msg
 }
 
+func buildFoolMessageWithHighPrefix(s, prefix string) string {
+	var msg string
+	for i := 1; i <= len(s); i++ {
+		d := s[len(s)-i]
+		n := number[d]
+
+		// 0 は発音しない
+		if d == '0' {
+			continue
+		}
+
+		var p string
+		switch i {
+		case 2: // 十の位
+			p = "ｼﾞｭｳ"
+
+			// 1 は発音しない
+			if d == '1' {
+				n = ""
+			}
+
+		case 3: // 百の位
+			p = "ﾋｬｸ"
+
+			// 1 は発音しない
+			if d == '1' {
+				n = ""
+			}
+
+			// 3のときは接頭辞の読み方が変わる
+			if d == '3' {
+				p = "ﾋﾞｬｸ"
+			}
+
+			// 6のときは特別な読み方をする
+			if d == '6' {
+				n = "ﾛｯ"
+				p = "ﾋﾟｬｸ"
+			}
+
+			// 8のときは特別な読み方をする
+			if d == '8' {
+				n = "ﾊｯ"
+				p = "ﾋﾟｬｸ"
+			}
+
+		case 4: // 千の位
+			p = "ｾﾝ"
+
+			// 1 の読み方が変わる
+			if d == '1' {
+				n = "ｲｯ"
+			}
+
+			// 3のときは接頭辞の読み方が変わる
+			if d == '3' {
+				p = "ｾﾞﾝ"
+			}
+
+			// 8のときは特別な読み方をする
+			if d == '8' {
+				n = "ﾊｯ"
+			}
+
+		}
+
+		msg = n + p + msg
+	}
+
+	// 表現すべき数字が何もなかった場合 (0 埋めのケース)
+	if msg == "" {
+		return ""
+	}
+
+	return msg + prefix
+}
+
 var number = map[byte]string{
 	'0': "",
 	'1': "ｲﾁ",
@@ -113,4 +209,24 @@ var number = map[byte]string{
 	'7': "ﾅﾅ",
 	'8': "ﾊﾁ",
 	'9': "ｷｭｳ",
+}
+
+var prefix = []string{
+	"ﾏﾝ",
+	"ｵｸ",
+	"ﾁｮｳ",
+	"ｹｲ",
+	"ｶﾞｲ",
+	"ｼﾞｮ",
+	"ｼﾞｮｳ",
+	"ｺｳ",
+	"ｶﾝ",
+	"ｾｲ",
+	"ｻｲ",
+	"ｺﾞｸ",
+	"ｺﾞｳｶﾞｼｬ",
+	"ｱｿｳｷﾞ",
+	"ﾅﾕﾀ",
+	"ﾌｶｼｷﾞ",
+	"ﾑﾘｮｳﾀｲｽｳ",
 }
